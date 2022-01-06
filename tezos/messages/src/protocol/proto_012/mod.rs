f121c852631835b56ab79056ec18877da66fa499
Bruno Deferrari <bruno.deferrari@viablesystems.io>
2022-01-06 18:24:42 -0300

    RPC and messages for proto 012 (WIP)

---
+++ tezos/messages/src/protocol/proto_012/mod.rs
@@ -0,0 +1,10 @@
+// Copyright (c) SimpleStaking, Viable Systems and Tezedge Contributors
+// SPDX-License-Identifier: MIT
+
+pub mod constants;
+pub mod contract;
+pub mod operation;
+pub mod rights;
+pub mod votes;
+
+pub const PROTOCOL_HASH: &str = "PsiThaCaT47Zboaw71QWScM8sXeMM7bbQFncK9FLqYc6EKdpjVP";
