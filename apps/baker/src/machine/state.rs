a71e3acb3b699e96f2f954a28d8ec8ac016f9466
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2022-02-11 09:38:24 +0200

    chore: use redux

--- apps/baker/src/machine/state.rs
+++ apps/baker/src/machine/state.rs
@@ -1,9 +1,56 @@
 // Copyright (c) SimpleStaking, Viable Systems and Tezedge Contributors
 // SPDX-License-Identifier: MIT
 
-use serde::{Deserialize, Serialize};
+use std::{time::Duration, collections::BTreeMap};
 
-#[derive(Default, Serialize, Deserialize)]
-pub struct State {
-    pub is_bootstrapped: bool,
+use chrono::{DateTime, Utc};
+
+use crypto::hash::{ChainId, BlockHash, ContractTz1Hash};
+use tezos_encoding::enc::BinWriter;
+use tezos_messages::protocol::proto_012::operation::InlinedEndorsementContents;
+
+use crate::types::ProtocolBlockHeader;
+
+#[derive(Debug)]
+pub enum State {
+    Initial,
+    RpcError(String),
+    ContextConstantsParseError,
+    GotChainId(ChainId),
+    Ready {
+        config: Config,
+        // TODO: rename
+        current_head_data: Option<BlockData>,
+    },
+}
+
+#[derive(BinWriter, Debug)]
+pub struct EndorsementUnsignedOperation {
+    pub branch: BlockHash,
+    pub content: InlinedEndorsementContents,
+}
+
+// TODO: rename
+#[derive(Debug)]
+pub struct BlockData {
+    pub slot: Option<u16>,
+    pub validators: BTreeMap<ContractTz1Hash, Vec<u16>>,
+    pub level: i32,
+    pub predecessor: BlockHash,
+
+    pub block_hash: BlockHash,
+    pub timestamp: DateTime<Utc>,
+    pub protocol_data: ProtocolBlockHeader,
+
+    pub seen_preendorsement: usize,
+    pub preendorsement: Option<EndorsementUnsignedOperation>,
+    pub endorsement: Option<EndorsementUnsignedOperation>,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    pub chain_id: ChainId,
+    pub quorum_size: usize,
+    pub minimal_block_delay: Duration,
+    pub delay_increment_per_round: Duration,
 }
