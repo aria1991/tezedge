ed9b8bc2c5ed54db9f9a75786fd43f6964df5c50
Zura Benashvili <zura.benashvili@gmail.com>
2022-02-15 00:14:02 +0400

    fix(shell_automaton): GetCurrentBranch and GetOperationsForBlocks p2p no response
    1. Fixed missing reducer/effects.
    2. Bugs in the enabling conditions for the get current branch request.

--- shell_automaton/src/actors/actors_effects.rs
+++ shell_automaton/src/actors/actors_effects.rs
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: MIT
 
 use crate::block_applier::BlockApplierEnqueueBlockAction;
-use crate::peer::message::write::PeerMessageWriteInitAction;
-use crate::peers::graylist::PeersGraylistAddressAction;
 use crate::peers::init::PeersInitAction;
 use crate::service::actors_service::ActorsMessageFrom;
 use crate::service::{ActorsService, Service};
@@ -21,35 +19,19 @@ pub fn actors_effects<S: Service>(store: &mut Store<S>, action: &ActionWithMeta)
                     ActorsMessageFrom::P2pInit => {
                         store.dispatch(PeersInitAction {});
                     }
-                    ActorsMessageFrom::PeerStalled(peer_id) => {
-                        // store.dispatch(PeersGraylistAddressAction {
-                        //     address: peer_id.address,
-                        // });
-                    }
-                    ActorsMessageFrom::BlacklistPeer(peer_id, _) => {
-                        // store.dispatch(PeersGraylistAddressAction {
-                        //     address: peer_id.address,
-                        // });
-                    }
-                    ActorsMessageFrom::SendMessage(peer_id, message) => {
-                        store.dispatch(PeerMessageWriteInitAction {
-                            address: peer_id.address,
-                            message,
-                        });
-                    }
                     ActorsMessageFrom::ApplyBlock {
-                        chain_id,
                         block_hash,
                         callback,
+                        ..
                     } => {
-                        // store
-                        //     .service
-                        //     .actors()
-                        //     .register_apply_block_callback(block_hash.clone(), callback);
-                        // store.dispatch(BlockApplierEnqueueBlockAction {
-                        //     chain_id,
-                        //     block_hash,
-                        // });
+                        store
+                            .service
+                            .actors()
+                            .register_apply_block_callback(block_hash.clone(), callback);
+                        store.dispatch(BlockApplierEnqueueBlockAction {
+                            block_hash,
+                            injector_rpc_id: None,
+                        });
                     }
                 }
             }
